ae7abee2216ff779d084c699bf4a80e2653e37bb
lzh-s <14301096@bjtu.edu.cn>
2016-09-18 17:44:15 +0800

    Add files via upload

---
+++ 14301096/14301096.py
@@ -0,0 +1,26 @@
+﻿import socket,thread
+from time import sleep
+
+def  Server(clientsocket,addr):
+    print ("CLIENTIP: %s" % str(addr))
+    msg = clientsocket.recv(1024)
+    print (msg)
+    client_message = msg
+    clientsocket.send(msg[::-1])
+    sleep(1024)
+    clientsocket.close()
+    print ("from %s connect is closed"% addr)
+    
+def main():
+    serversocket = socket.socket() 
+    host = socket.gethostname()
+    port = 9999
+    serversocket.bind((host, port))
+    serversocket.listen(20)
+    print ("serversocket is running,waiting client connect...")
+    while True:
+        clientsocket,addr = serversocket.accept()
+        thread.start_new_thread(MutiServer,(clientsocket,addr))
+        
+if __name__ == '__main__':
+    main()
